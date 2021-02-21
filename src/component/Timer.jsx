ddfd556105ba1515eb8271b29f5bbbfb498ebd59
Edouard Lacan <edouard.lacan@yoomap.fr>
2021-02-21 23:37:30 +0100

    Add date and twitter metas

--- src/component/Timer.jsx
+++ src/component/Timer.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 
 export default function Timer() {
-    // const locale = 'fr';
+    const locale = 'fr';
     const [today, setDate] = useState(new Date()); // Save the current date to be able to trigger an update
 
     useEffect(() => {
@@ -15,15 +15,15 @@ export default function Timer() {
       }
     }, []);
 
-    // const day = today.toLocaleDateString(locale, { weekday: 'long' });
-    // const date = `${day}, ${today.getDate()} ${today.toLocaleDateString(locale, { month: 'long' })}\n\n`;
+    const day = today.toLocaleDateString(locale, { weekday: 'long' });
+    const date = `${day}, ${today.getDate()} ${today.toLocaleDateString(locale, { month: 'long' })}\n\n`;
 
     const hour = today.getHours();
     const minute = today.getMinutes();
 
     const phrase = [
       "le couvre feu. Reste chez toi !",
-      "bon là tu peux te ballader",
+      "bon là tu peux te balader",
       "bientôt le couvre feu, hâte toi !",
       "le couvre feu. Reste chez toi !"
     ]
@@ -35,11 +35,12 @@ export default function Timer() {
       ( (18 > hour && 44 < minute) && phrase[2]) ||
       phrase[3]}`;
 
-    // const time = today.toLocaleTimeString(locale, { hour: 'numeric', hour24: true, minute: 'numeric', second: 'numeric' });
+    const time = today.toLocaleTimeString(locale, { hour: 'numeric', hour24: true, minute: 'numeric', second: 'numeric' });
 
     return (
       <div>
-        <h3>{ wish }</h3>
+        <h4>{ date } { time }</h4>
+        <h2>{ wish }</h2>
       </div>
     );
   };
